56d407662d15a81b9c27c86503a666ad8949b36f
naga oleti <venu.babu80@gmail.com>
2020-01-12 21:55:11 +0400

    second commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import sqlite3
+import json
+from pathlib import Path
+
+movies = json.loads(Path("movies.json").read_text())
+
+# print(movies)
+# with sqlite3.connect("db.sqlite3") as conn:
+#     command = "insert into Movies values (?,?,?)"
+#     for movie in movies:
+#         conn.execute(command, tuple(movie.values()))
+#     conn.commit()
+
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "select * from Movies"
+    cursor = conn.execute(command)
+    # for row in cursor:
+    #     print(row)
+    movies = cursor.fetchall()
+    print(movies)
